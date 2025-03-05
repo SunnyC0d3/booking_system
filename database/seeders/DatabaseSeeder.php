257a3b9e4394caadafa00b2fcde1d8c072acb0bb
Sukhwinder Singh <s.sunny_1995@hotmail.co.uk>
2025-03-05 16:27:54 +0000

    starting Orders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,26 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             RoleSeeder::class,
             PermissionSeeder::class,
             RolePermissionSeeder::class,
+
+            OrderStatusSeeder::class,
+            OrderRefundStatusSeeder::class,
+            OrderReturnStatusSeeder::class,
+            OrderShipmentStatusSeeder::class,
+
             UserSeeder::class,
             VendorSeeder::class,
+
             ProductStatusSeeder::class,
             ProductCategorySeeder::class,
             ProductAttributeSeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+
+            OrderSeeder::class,
         ]);
     }
 }
