f8e458b6d0568c3dc1755a48c43c6feecc87b184
Sukhwinder Singh <s.sunny_1995@hotmail.com>
2025-06-20 14:30:04 +0100

    seperated classes

--- api/app/Constants/PasswordRequirements.php
+++ api/app/Constants/PasswordRequirements.php
@@ -32,20 +32,3 @@ class PasswordRequirements
 
     public const PASSWORD_HISTORY_COUNT = 5;
 }
-
-class AccountLockSettings
-{
-    public const MAX_LOGIN_ATTEMPTS = 5;
-    public const LOCKOUT_DURATION_MINUTES = 30;
-    public const PROGRESSIVE_LOCKOUT_ENABLED = true;
-
-    public const LOCKOUT_DURATIONS = [
-        1 => 5,   // 1st lockout: 5 minutes
-        2 => 15,  // 2nd lockout: 15 minutes
-        3 => 30,  // 3rd lockout: 30 minutes
-        4 => 60,  // 4th lockout: 1 hour
-        5 => 240, // 5th+ lockout: 4 hours
-    ];
-
-    public const RESET_ATTEMPTS_AFTER_HOURS = 24;
-}
