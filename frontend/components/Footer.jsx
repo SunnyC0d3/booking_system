94882552f26c895dc8f4cfe3470094a97044c500
Sukhwinder Singh <s.sunny_1995@hotmail.com>
2025-07-01 18:34:10 +0100

    deleted frontend

--- frontend/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="text-center py-6 text-gray-500 text-sm border-t">
-            &copy; {new Date().getFullYear()} TemplateHub. All rights reserved.
-        </footer>
-    );
-}
-
-export default Footer;
