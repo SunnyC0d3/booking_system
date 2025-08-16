ff0c8068dffd717c36998611610389953fc9370d
Sukhwinder Singh <s.sunny_1995@hotmail.com>
2025-08-16 13:26:46 +0100

    Turning system into service based system

--- frontend/src/components/digital/index.ts
+++
@@ -1,6 +0,0 @@
-export { DigitalLibrary } from './DigitalLibrary';
-export { DownloadProgress } from './DownloadProgress';
-export { LicenseManager } from './LicenseManager';
-export { DigitalProductCard } from './DigitalProductCard';
-export { DownloadHandler } from './DownloadHandler';
-export { DigitalProductInfo } from './DigitalProductInfo';
